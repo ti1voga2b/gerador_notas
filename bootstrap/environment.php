ead62716bb16a57e6dc3b028e55b0ec4cf097a31
lsmoraes22 <lucassangirato@gmail.com>
2026-03-26 15:18:32 -0300

    log commit

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -1,10 +1,19 @@
 <?php
 
 $envPath = dirname(__DIR__) . '/.env';
+$logDirectory = dirname(__DIR__) . '/storage/logs';
+$logFile = $logDirectory . '/app.log';
 
 if (class_exists('Env')) {
     Env::load($envPath);
 }
+
+if (!is_dir($logDirectory)) {
+    mkdir($logDirectory, 0777, true);
+}
+
+ini_set('log_errors', '1');
+ini_set('error_log', $logFile);
 
 function app_base_path()
 {
